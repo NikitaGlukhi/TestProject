ed5bf7eb1ef4ccb8873536341ea118514139c263
NikitaGlukhi <nikita.glukhi@mail.ru>
2016-11-15 15:53:30 +0200

    Update Routes.ts

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -14,13 +14,17 @@ import {ResultComponent} from "./Results";
 import {Results1Component} from "./Results1";
 
 const routes: Routes = [
-
+  {
   { path: 'contact', component: ContactComponent },
   { path: 'mathematics', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'results/:res/:unres/:n/:sn', component: ResultComponent},
   { path: 'results1/:res1/:unres1', component: Results1Component},
-  { path: '', component: ContactComponent }
+  { path: '', component: ContactComponent, data:{'home': true} }
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
 ];
-
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
