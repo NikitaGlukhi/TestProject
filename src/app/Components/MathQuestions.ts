99e6885edad6c7af44b648617cc5efb3bfac5793
NikitaGlukhi <nikita.glukhi@mail.ru>
2017-02-09 09:41:09 +0200

    Test1Commit

---
+++ src/app/Components/MathQuestions.ts
@@ -0,0 +1,321 @@
+/**
+ * Created by nikita_glukhi on 2/7/17.
+ */
+export const Mathematics = [
+    {
+      name: 'Задание 1',
+      number1: 1,
+      text: '<i>5x <sup><small>3</small></sup> - 8x <sup><small>2</small></sup> - 8x + 5 = 0 </i>',
+      answers: [
+    {
+      value1: 1,
+      letter1:'А)',
+      print1: 'x = 0'
+    },
+
+    {
+      value1: 2,
+      letter1:'Б)',
+      print1: 'x = &#8731;<span>3</span> &#8260; &#8732;<span>2</span>'
+    },
+
+    {
+      value1: 3,
+      letter1:'В)',
+      print1: 'x = 13 &#8260; 10 + &#8730;<span>69</span> &#8260; 10; x = 13 &#8260; 10 - &#8730;<span>69</span> &#8260; 10; x = -1'
+    },
+
+    {
+      value1: 4,
+      letter1:'Г)',
+      print1: 'x = 13 &#8260; &#8730;10 + &#8731;69 &#8260; 10; x = 1'
+    },
+  ]
+  },
+
+  {
+    name: 'Задание 2',
+    number2: 2,
+    text: 'На какой картинке изобраажен график функции y = Ax<sup><small>2</small></sup> + Bx + C? КОЭФФИЦИЕНТЫ: A > 0; C > 0',
+    image1:'http://otvet-gotov.ru/pages/images/0264AF-2.png',
+    image2:'http://otvet-gotov.ru/pages/images/0264AF.png',
+    image3:'http://otvet-gotov.ru/pages/images/0264AF-1.png',
+    image4:'http://otvet-gotov.ru/pages/images/0FB73A-2.png',
+    answers: [
+      {
+        value2: 1,
+        letter2:'А)',
+        print2: '1'
+      },
+
+      {
+        value2: 2,
+        letter2:'Б)',
+        print2: '2'
+      },
+
+      {
+        value2: 3,
+        letter2:'В)',
+        print2: '3'
+      },
+
+      {
+        value2: 4,
+        letter2:'Г)',
+        print2: '4'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 3',
+    number3: 3,
+    text: 'В треугольной усеченной пирамиде с высотой, равной 10, стороны одного из оснований равны 27, 29 и 52. Определите объем усеченной пирамиды, если периметр другого основания равен 72.',
+    image5:'http://testmath.ru/wp-content/uploads/stereometria/10/image002.jpg',
+    answers: [
+      {
+        value3: 1,
+        letter3:'А)',
+        print3: '1700'
+      },
+
+      {
+        value1: 2,
+        letter1:'Б)',
+        print3: '1450'
+      },
+
+      {
+        value3: 3,
+        letter3:'В)',
+        print3: '1940'
+      },
+
+      {
+        value3: 4,
+        letter3:'Г)',
+        print3: '1900'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 4',
+    number4: 4,
+    text: 'Решить неравенство <i>x<sup><small>2</small></sup> - 15x + 50 > 0</i>',
+    answers: [
+      {
+        value4: 1,
+        letter4:'А)',
+        print4: 'x &#8712; (-&#8734; ; -4) &#8746; (5; &#8734;)'
+      },
+
+      {
+        value4: 2,
+        letter4:'Б)',
+        print4: 'x &#8712; (-&#8734; ; -4] &#8746; [5; &#8734;)'
+      },
+
+      {
+        value4: 3,
+        letter4:'В)',
+        print4: 'x &#8712;  &#8709;'
+      },
+
+      {
+        value4: 4,
+        letter4:'Г)',
+        print4: 'x &#8712; (-&#8734; ; &#8734;)'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 5',
+    number5: 5,
+    text: 'Найдите значение выражения: log<sub><small>5</small></sub>0,2 + log<sub><small>0,5</small></sub>4',
+    answers: [
+      {
+        value5: 1,
+        letter5:'А)',
+        print5: '3'
+      },
+
+      {
+        value5: 2,
+        letter5:'Б)',
+        print5: '-3'
+      },
+
+      {
+        value5: 3,
+        letter5:'В)',
+        print5: '1'
+      },
+
+      {
+        value5: 4,
+        letter5:'Г)',
+        print5: '2'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 6',
+    number6: 6,
+    text: 'На рисунке изображен график некоторой функции y = &#402;(x). Пользуясь им, вычислите интеграл <sub><small>1</small></sub><sup><small>5</small></sup>&#8747; = &#402;(x) dx.',
+    image6: 'https://ege.sdamgia.ru/get_file?id=6706',
+    answers: [
+      {
+        value6: 1,
+        letter6:'А)',
+        print6: '11'
+      },
+
+      {
+        value6: 2,
+        letter6:'Б)',
+        print6: '12'
+      },
+
+      {
+        value6: 3,
+        letter6:'В)',
+        print6: '13'
+      },
+
+      {
+        value6: 4,
+        letter6:'Г)',
+        print6: '14'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 7',
+    number7: 7,
+    text: 'Решить уравнение (-&#8730;2)sin(-5&#960; &#8260; 2 + x) * sinx = cosx и найти все его заначения на промежутке &#91; 9&#960; &#8260; 2 , 6&#960; &#93;.',
+    answers: [
+      {
+        value7: 1,
+        letter7:'А)',
+        print7: '&#123; -(&#960; &#8260; 3) + 2&#960;k; -(&#960; &#8260; 2) + 2&#960;k; : k &#8712; Z &#125;'
+      },
+
+      {
+        value7: 2,
+        letter7:'Б)',
+        print7: '&#123; &#960; &#8260; 6 + 2&#960;k; 5&#960; &#8260; 6 + 2&#960;k; : k &#8712; Z &#125;'
+      },
+
+      {
+        value7: 3,
+        letter7:'В)',
+        print7: '&#123; &#960; &#8260; 2 + &#960;k; -(&#960; &#8260; 4) + 2&#960;k; 3&#960; -(3&#8260; 4) + 2&#960;k : k &#8712; Z &#125;'
+      },
+
+      {
+        value7: 4,
+        letter7:'Г)',
+        print7: '&#123; &#960; &#8260; 2 + &#960;k; &#960; &#8260; 4 + 2&#960;k; 3&#960; &#8260; 4 + 2&#960;k : k &#8712; Z &#125;'
+      },
+    ]
+  },
+
+
+  {
+    name: 'Задание 8',
+    number8: 8,
+    text: 'Решить уравнение 7<sup><small>18,5x + 0,7</small></sup> = 1 &#8260; 343.',
+    answers: [
+      {
+        value8: 1,
+        letter8:'А)',
+        print8: 'x = -0,22'
+      },
+
+      {
+        value8: 2,
+        letter8:'Б)',
+        print8: 'x = -0,2'
+      },
+
+      {
+        value8: 3,
+        letter8:'В)',
+        print8: '0,2'
+      },
+
+      {
+        value8: 4,
+        letter8:'Г)',
+        print8: '1'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 9',
+    number9: 9,
+    text: 'В треугольнике АВС &#8736;С = 90&#176;, AB = 5, sin&#8736;A = 7 &#8260; 25. Найдите АС.',
+    answers: [
+      {
+        value9: 1,
+        letter9:'А)',
+        print9: '4'
+      },
+
+      {
+        value9: 2,
+        letter9:'Б)',
+        print9: '4,6'
+      },
+
+      {
+        value9: 3,
+        letter9:'В)',
+        print9: '4,8'
+      },
+
+      {
+        value9: 4,
+        letter9:'Г)',
+        print9: '5'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 10',
+    number10: 10,
+    text: 'Прик аких значениях параметра а, уравнение x<sup><small>2</small></sup> + (1 - a<sup><small>2</small></sup>) = &#124;x - 1 + a&#124; + &#124;x - a + 1&#124; имеет единственный корень?',
+    answers: [
+      {
+        value10: 1,
+        letter10:'А)',
+        print10: 'При а = -2 и а = 3'
+      },
+
+      {
+        value10: 2,
+        letter10:'Б)',
+        print10: 'При а = -1 и а = -3'
+      },
+
+      {
+        value10: 3,
+        letter10:'В)',
+        print10: 'При а = 1 и а = -3'
+      },
+
+      {
+        value10: 4,
+        letter10:'Г)',
+        print10: 'При а = -1 и а = 3'
+      },
+    ]
+  }
+];
---
+++ src/app/Components/PhysicsQuestiont.ts
@@ -0,0 +1,310 @@
+/**
+ * Created by nikita_glukhi on 2/9/17.
+ */
+export const PysicsQuestions = [
+  {
+    name: 'Задание 1',
+    number11: 13,
+    formControl: 'searchControl13',
+    text: 'Из открытого люка автомобиля, который движется прямолинейно и равномерно со скоростью 25 м/с в направлении, противоположном движению, горизонтально бросают мячик со скоростью 5 м/с относительно автомобиля. Какова скорость этого мячика относительно Земли?',
+    answers: [
+      {
+        value11: 1,
+        letter11:'А)',
+        print1: '5 м/с'
+      },
+
+      {
+        value11: 2,
+        letter11:'Б)',
+        print11: '20 м/с'
+      },
+
+      {
+        value11: 3,
+        letter11:'В)',
+        print11: '25 м/с'
+      },
+
+      {
+        value11: 4,
+        letter11:'Г)',
+        print11: '30 м/с'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 2',
+    number12: 14,
+    formControl: 'searchControl12',
+    text: 'Какой из приведенных ниже графиков правильно показывает зависимость потенциальной энергии этого тела относительно поверхности Земли от времени?',
+    image7: 'https://ege.yandex.ru/media/ege/physics/v3/ege-physics-v3-a5-q.png',
+
+    answers: [
+      {
+        value12: 1,
+        letter12:'А)',
+        print12: '1'
+      },
+
+      {
+        value12: 2,
+        letter12:'Б)',
+        print12: '2'
+      },
+
+      {
+        value12: 3,
+        letter12:'В)',
+        print12: '3'
+      },
+
+      {
+        value12: 4,
+        letter12:'Г)',
+        print12: '4'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 3',
+    number13: 15,
+    formControl: 'searchControl13',
+    text: 'Какую работу совершает газ в замкнутом процессе 1→2→3→4→5→3→1?',
+    image8: 'https://ege.yandex.ru/media/ege/physics/v3/ege-physics-v3-a8-t.png',
+    answers: [
+      {
+        value13: 1,
+        letter13:'А)',
+        print13: 'Положительную'
+      },
+
+      {
+        value13: 2,
+        letter13:'Б)',
+        print13: 'Отрицательную'
+      },
+
+      {
+        value13: 3,
+        letter13:'В)',
+        print13: 'Равную нулю'
+      },
+
+      {
+        value13: 4,
+        letter13:'Г)',
+        print13: 'Однозначно ответить нельзя'
+      },
+    ]
+  },
+
+  {
+    name:'Задание 4',
+    number14: 16,
+    formControl: 'searchControl14',
+    text: 'Свет доходит от Солнца до Земли за 500 с, а от Солнца до Марса — за 760 с. Чему равно наименьшее расстояние от Земли до Марса при их движении по своим орбитам? Справочные данные: расстояние между Землёй и Солнцем 1,5⋅10<sup><small>11</small></sup> м.',
+    answers: [
+      {
+        value14: 1,
+        letter14:'А)',
+        print14: '78  млн. км'
+      },
+      {
+        value14: 2,
+        letter14:'Б)',
+        print14: '150  млн. км'
+      },
+      {
+        value14: 3,
+        letter14:'В)',
+        print14: '228  млн. км'
+      },
+      {
+        value14: 4,
+        letter14:'Г)',
+        print14: '378  млн. км'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 5',
+    number15: 17,
+    formControl: 'searchControl15',
+    text: 'На каком из приведенных рисунков правильно изображена схема атома азота <sup><small>14</small></sup><sub><small>7</small></sub>N?',
+    image9: 'https://ege.yandex.ru/media/ege/physics/v3/ege-physics-v3-a17-t.png',
+    answers: [
+      {
+        value15: 1,
+        letter15:'А)',
+        print15: '1'
+      },
+      {
+        value15: 2,
+        letter15:'Б)',
+        print15: '2'
+      },
+      {
+        value15: 3,
+        letter15:'В)',
+        print15: '3'
+      },
+      {
+        value15: 4,
+        letter15:'Г)',
+        print15: '4'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 6',
+    number16: 18,
+    formControl: 'searchControl16',
+    text: 'Неизвестным продуктом X ядерной реакции <sup><small>27</small></sup><sub><small>13</small></sub>Al+<sup><small>1</small></sup><sub><small>0</small></sub>n→<sup><small>24</small></sup><sub><small>11</small></sub>Na+X является...',
+    answers: [
+      {
+        value16: 1,
+        letter16:'А)',
+        print16: '&#945; - частица'
+      },
+      {
+        value16: 2,
+        letter16:'Б)',
+        print16: '&#946; - частица'
+      },
+      {
+        value16: 3,
+        letter16:'В)',
+        print16: '&#947; - частица'
+      },
+      {
+        value16: 4,
+        letter16:'Г)',
+        print16: 'Электрон'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 7',
+    number17: 19,
+    formControl: 'searchControl17',
+    text: 'В закрытом сосуде находится одноатомный газ неон, имеющий молярную массу 20г/моль. Его температуру увеличивают от 338K до 512K. Считая, что в этом процессе неон можно считать идеальным газом, найдите, как и на сколько в результате изменится среднеквадратичная скорость атомов неона? Справочные данные: универсальная газовая постоянная R=8,31Дж/(моль⋅К).',
+    answers: [
+      {
+        value17: '1',
+        letter17:'А)',
+        print17: 'Уменьшится на 150 м/c.'
+      },
+      {
+        value17: 2,
+        letter17:'Б)',
+        print17: 'Увеличится на 150 м/c.'
+      },
+      {
+        value17: 3,
+        letter17:'В)',
+        print17: 'Увеличится на 800 м/с.'
+      },
+      {
+        value17: 4,
+        letter17:'Г)',
+        print17: 'Уменьшится на 650 м/с.'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 8',
+    number18: 20,
+    formControl: 'searchControl18',
+    text: 'Сила тока в цепи идеального колебательного контура изменяется по закону I=0,2sin(2⋅10<sup><small>5</small></sup>⋅t), где сила тока I в амперах, время t в секундах. Чему равна емкость конденсатора, входящего в состав этого колебательного контура, если индуктивность катушки этого контура равна  2⋅10<sup><small>-2</small></sup> Гн?',
+    answers: [
+      {
+        value18: 1,
+        letter18:'А)',
+        print18: '12,5 мФ'
+      },
+      {
+        value18: 2,
+        letter18:'Б)',
+        print18: '0,25 мФ'
+      },
+      {
+        value18: 3,
+        letter18:'В)',
+        print18: '1,25 нФ'
+      },
+      {
+        value18: 4,
+        letter18:'Г)',
+        print18: '0,1 мкФ'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 9',
+    number19: 21,
+    formControl: 'searchControl19',
+    text: `Объем идеального газа массой 4,5 кг составляет 6,4 м<sup><small>3</small></sup>. Давление газа на стенки сосуда равно 150 кПа. Какова средняя квадратичная скорость молекул этого газа?`,
+    answers: [
+      {
+        value19: 1,
+        letter19:'А)',
+        print19: '200 м/c'
+      },
+      {
+        value19: 2,
+        letter19:'Б)',
+        print19: '400 м/c'
+      },
+      {
+        value19: 3,
+        letter19:'В)',
+        print19: '600 м/c'
+      },
+      {
+        value19: 4,
+        letter19:'Г)',
+        print19: '800 м/c'
+      },
+    ]
+  },
+
+  {
+    name: 'Задание 10',
+    number20: 22,
+    formControl: 'searchControl20',
+    text: 'На каком графике отображена зависимость заряда конденсатора от напряжения, приложенного к его пластинам?',
+    image10: 'https://ege.yandex.ru/media/zno-ru/physics/9/10/choice1_1_zno_2015_phys_9_10_1.png',
+    image11: 'https://ege.yandex.ru/media/zno-ru/physics/9/10/choice2_1_zno_2015_phys_9_10_2.png',
+    image12: 'https://ege.yandex.ru/media/zno-ru/physics/9/10/choice3_1_zno_2015_phys_9_10_3.png',
+    answers: [
+      {
+        value20: 1,
+        letter20:'А)',
+        print20: '1'
+      },
+      {
+        value20: 2,
+        letter20:'Б)',
+        print20: '2'
+      },
+      {
+        value20: 3,
+        letter20:'В)',
+        print20: '3'
+      },
+      {
+        value20: 4,
+        letter20:'Г)',
+        print20: 'Все графики неверны'
+      },
+    ]
+  },
+];
